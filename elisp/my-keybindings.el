163b7079490e03581309b10de0c71b82467bef42
d-rezzer <ryanlmartin@gmail.com>
2015-10-22 16:25:44 -0600

    added key binding for buffer-move

--- elisp/my-keybindings.el
+++ elisp/my-keybindings.el
@@ -12,3 +12,12 @@
 ;; I like to put my special sauce under C-,
 (global-set-key (kbd "C-, e") 'eshell)
 (global-set-key (kbd "C-, z") 'shell)
+(global-set-key (kbd "C-, p") 'list-packages)
+
+(global-set-key (kbd "C-<left>") 'buf-move-left)
+(global-set-key (kbd "C-<right>") 'buf-move-right)
+(global-set-key (kbd "C-<up>") 'buf-move-up)
+(global-set-key (kbd "C-<down>") 'buf-move-down)
+
+
+;;(global-set-key (kbd "") 'some-cmd)
